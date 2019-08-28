c95e4853cb0e4d36a9f213d289cbd18ff8a0008e
unknown <Deepak.Singh@surebot.co>
2019-08-28 17:16:34 +0530

    new Request Added

--- app/src/main/java/service/com/surebot/info/serviceperson/Constants/Constants.java
+++ app/src/main/java/service/com/surebot/info/serviceperson/Constants/Constants.java
@@ -5,10 +5,10 @@ public class Constants {
     public static String TOKEN="a28c0c07408e9a36d34d44510eeb1a66";
     //public static final String BASE_URL = "https://hsqa.surebotdev.com/index.php/Api/";
 
-    public static final String BASE_URL = "http://192.168.0.82/services_at_home_test/Api/";
+    public static final String BASE_URL = "http://192.168.0.86/services_at_home/Api/";
 
 
 
 
-    public static final String IMAGEBASE_URL="http://192.168.0.82/services_at_home/";
+    public static final String IMAGEBASE_URL="http://192.168.0.86/services_at_home/";
 }
