5f78b3de2be233e58288b923251535b6de759b36
SnehaMadda <sneha.m@surebot.co>
2020-07-02 18:59:56 +0530

    Add patner signup and login

---
+++ app/src/main/java/service/com/surebot/info/serviceperson/Activity/PartnerLoginActivity.java
@@ -0,0 +1,15 @@
+package service.com.surebot.info.serviceperson.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import service.com.surebot.info.serviceperson.R;
+
+import android.os.Bundle;
+
+public class PartnerLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partner_login);
+    }
+}
---
+++ app/src/main/java/service/com/surebot/info/serviceperson/Activity/PartnerSignupActivity.java
@@ -0,0 +1,22 @@
+package service.com.surebot.info.serviceperson.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import service.com.surebot.info.serviceperson.R;
+
+public class PartnerSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partner_signup);
+
+    }
+}
